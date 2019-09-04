cc498de2a4651f2408052b4f4a714c6b0a16da41
dinesh.badavath <dinesh.badavath@cgi.com>
2019-09-04 12:14:16 +0530

    Isogram_solved

---
+++ Isogram.Tests/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using isogram_csharp_problem;
+using Xunit;
+
+namespace Isogram.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void isogramcheck_emptyString()
+        {
+            Assert.Equal(true,Program.isogram(""));
+        }
+        [Fact]
+        public void isogramcheck_lowercase()
+        {
+            Assert.Equal(true, Program.isogram("isogram"));
+        }
+        [Fact]
+        public void isogramcheck_eleven()
+        {
+            Assert.Equal(false, Program.isogram("eleven"));
+        }
+        [Fact]
+        public void isogramcheck_longEnglishWords()
+        {
+            Assert.Equal(true, Program.isogram("subdermatoglyphic"));
+        }
+        [Fact]
+        public void isogramcheck_duplicateCharactersMixedCase()
+        {
+            Assert.Equal(true, Program.isogram("Alphabet"));
+        }
+        [Fact]
+        public void isogramcheck_hyphen()
+        {
+            Assert.Equal(true, Program.isogram("thumbscrew-japingly"));
+        }
+        [Fact]
+        public void isogramcheck_duplicateHyphen()
+        {
+            Assert.Equal(true, Program.isogram("six-year-old"));
+        }
+        [Fact]
+        public void isogramcheck_spaces()
+        {
+            Assert.Equal(true, Program.isogram("take m b"));
+        }
+    }
+}
---
+++ isogram-csharp-problem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+        public static bool isogram(string text)
+        {
+            int len = text.Length;
+
+            char[] arr = text.ToCharArray();
+
+            Array.Sort(arr);
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (arr[i] != ' ' && arr[i] != '-')
+                {
+                    if (arr[i] == arr[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return true;
+        }
+    }
+}
